6e3a9a4dacfe83f74cd4550deb5d75a08cd6eb5f
shabihhaider <2022cs723@student.uet.edu.pk>
2024-11-24 06:45:42 -0800

    Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary']; // So that it can protect against mass assignment attacks (what columns are allowed to be mass assigned)
 }

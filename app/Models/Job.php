67eef59a0ad3850a25e2d9a52bb5005550cd4600
shabihhaider <2022cs723@student.uet.edu.pk>
2024-11-24 09:30:50 -0800

    two key eloquent relationship types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,4 +9,9 @@ class Job extends Model {
     use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary']; // So that it can protect against mass assignment attacks (what columns are allowed to be mass assigned)
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }

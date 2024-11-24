f8d134e54f88c43424a5630fdff637909384f9be
shabihhaider <2022cs723@student.uet.edu.pk>
2024-11-23 18:41:39 -0800

    Autoloading, Namespace and Models

---
+++ app/Models/Job.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job {
+    public static function all() : array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Programmer',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Director',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Designer',
+                'salary' => '$30,000'
+            ]
+        ];
+    }
+
+    public static function find($id) : array
+    {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if (!$job) {
+            abort(404);
+        }
+
+        return $job;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Job;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,54 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function(){
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/job/{id}', function($id){
     
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Programmer',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Director',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Designer',
-                'salary' => '$30,000'
-            ]
-        ];
-    
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    
+    $job = Job::find($id);
+  
     return view("job", ['job' => $job]);
 });
 

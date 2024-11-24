870d26c5964c30fa14479f6a34c0e5f03156467a
shabihhaider <2022cs723@student.uet.edu.pk>
2024-11-24 02:31:52 -0800

    Eloquent (OMR) (create(),all(),find()...)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all() : array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Programmer',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Director',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Designer',
-                'salary' => '$30,000'
-            ]
-        ];
-    }
-
-    public static function find($id) : array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary']; // So that it can protect against mass assignment attacks (what columns are allowed to be mass assigned)
 }
